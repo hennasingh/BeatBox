aa799425c023662331b271c7aa3058b36b9cb945
WEBARTIST\User <hennasingh04@gmail.com>
2018-10-06 13:56:41 +0100

    addded recycler view with grid view, created assets folder with .wav files and button in the grid display names of the files. hidden bug- the recycler view display repeated items.

---
+++ app/src/main/java/com/artist/web/beatbox/BeatBox.java
@@ -0,0 +1,44 @@
+package com.artist.web.beatbox;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BeatBox {
+
+    private static final String TAG = "BeatBox";
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+    private List<Sound> mSounds = new ArrayList<>();
+    private String[] soundNames;
+
+    private AssetManager mAssets;
+
+    BeatBox(Context context) {
+        mAssets = context.getAssets();
+        loadSounds();
+    }
+
+    private void loadSounds() {
+
+        try {
+            soundNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found " + soundNames.length + " sounds");
+        } catch (IOException ioe) {
+            Log.e(TAG, "Could not list assets", ioe);
+        }
+
+        for (String fileName : soundNames) {
+            String assetPath = SOUNDS_FOLDER + "/" + fileName;
+            Sound sound = new Sound(assetPath);
+            mSounds.add(sound);
+        }
+    }
+
+    public List<Sound> getSounds() {
+        return mSounds;
+    }
+}
